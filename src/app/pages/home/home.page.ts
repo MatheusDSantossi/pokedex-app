f9f5eff4f562a72a991c10f20920350862735726
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-06-20 11:11:20 -0300

    feat(favorites): melhorar integração com favoritos adicionando novas funcionalidades

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,31 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ViewWillEnter } from '@ionic/angular';
 import {
   PokemonService,
   PokemonListItem,
 } from '../../core/services/pokemon.service';
 import { addIcons } from 'ionicons';
-import { sunny, moon, arrowForward, arrowBack, heart, heartOutline } from 'ionicons/icons';
+import {
+  sunny,
+  moon,
+  arrowForward,
+  arrowBack,
+  heart,
+  heartOutline,
+} from 'ionicons/icons';
 import { ThemeToggleComponent } from 'src/app/shared/theme-toggle.componet';
 import { FavoritesService } from 'src/app/core/services/favorites.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterModule, ThemeToggleComponent],
+  imports: [IonicModule, CommonModule, RouterModule, FormsModule, ThemeToggleComponent],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, ViewWillEnter {
   pokemons: PokemonListItem[] = [];
   next: string | null = null;
   previous: string | null = null;
   // Estado local do tema
   isDarkMode: boolean = false;
 
+  // Novo controle de paginação
+  offset = 0;
+  readonly limit = 20;
+
   favSet = new Set<string>();
+
+  segment: 'all' | 'favorites' = 'all';
 
   constructor(
     private pokemonService: PokemonService,
@@ -33,8 +47,12 @@ export class HomePage implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.loadFavorites();
     this.loadPage();
+  }
+
+  // Este irá rodar toda vez que a página entra em foco
+  ionViewWillEnter(): void {
+    this.loadFavorites();
   }
 
   private loadFavorites() {
@@ -59,7 +77,7 @@ export class HomePage implements OnInit {
       : new URL(`${this.pokemonService['API']}/pokemon?offset=0&limit=20`)
           .searchParams;
 
-    const offset = params.get('offse  t') ?? '0';
+    const offset = params.get('offset') ?? '0';
     const limit = params.get('limit') ?? '20';
 
     this.pokemonService.listPokemons(+offset, +limit).subscribe((res) => {
@@ -72,4 +90,13 @@ export class HomePage implements OnInit {
 
     addIcons({ sunny, moon, arrowForward, arrowBack, heart, heartOutline });
   }
+
+  // Retorna a lista que deve ser renderiazada no *ngFor
+  get displayedPokemons(): PokemonListItem[] {
+    return this.segment === 'favorites' ? this.pokemons.filter(p => this.favSet.has(p.name)) :
+    this.pokemons;
+  }
+
+  // liga/desliga o filtro de favoritos
+  segmentChanged() {}
 }
